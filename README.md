7b54d3fe1c06593b473ce52e67d202e8779da874
Jauhen Feldman <feldmeow@gmail.com>
2022-02-11 21:18:17 +0300

    add lesson_01

---
+++ README.md
@@ -0,0 +1,1 @@
+#module_01 Method JS course
---
+++ lesson_01/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const goodName = 'Ноутбук',
+	quantityOfGoods = 10,
+	categoryOfGoods = 'Наземный транспорт',
+	goodPrice = 30000;
+
+console.log('Название товара : ' + goodName);
+console.log('Общая сумма товара : ' + quantityOfGoods * goodPrice);
