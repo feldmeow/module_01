e3644dced06021587fea26cefa6663b01a4a3c10
Jauhen Feldman <feldmeow@gmail.com>
2022-02-11 21:26:03 +0300

    add lesson_02

--- lesson_01/script.js
+++ lesson_01/script.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const goodName = 'Ноутбук',
-	quantityOfGoods = 10,
-	categoryOfGoods = 'Наземный транспорт',
-	goodPrice = 30000;
+{
+	const goodName = 'Ноутбук',
+		quantityOfGoods = 10,
+		categoryOfGoods = 'Наземный транспорт',
+		goodPrice = 30000;
 
-console.log('Название товара : ' + goodName);
-console.log('Общая сумма товара : ' + quantityOfGoods * goodPrice);
+	console.log('Название товара : ' + goodName);
+	console.log('Общая сумма товара : ' + quantityOfGoods * goodPrice);
+}
+
+{
+	const goodName = 'Монитор',
+		quantityOfGoods = 5,
+		categoryOfGoods = 'Наземный транспорт',
+		goodPrice = 3000;
+
+	console.log('Название товара : ' + goodName);
+	console.log('Общая сумма товара : ' + quantityOfGoods * goodPrice);
+}
