616aca3b19201e276ce7fe704604dd8024e2e9f1
Jauhen Feldman <feldmeow@gmail.com>
2022-02-14 19:01:29 +0300

    copy files from lesson01-02

---
+++ lesson03/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+	const goodName = 'Ноутбук',
+		quantityOfGoods = 10,
+		categoryOfGoods = 'Компьютеры',
+		goodPrice = 30000;
+
+	console.log('Название товара : ' + goodName);
+	console.log('Общая сумма товара : ' + quantityOfGoods * goodPrice);
+}
+
+{
+	const goodName = 'Монитор',
+		quantityOfGoods = 5,
+		categoryOfGoods = 'Комплектующие к компьютеру',
+		goodPrice = 3000;
+
+	console.log('Название товара : ' + goodName);
+	console.log('Общая сумма товара : ' + quantityOfGoods * goodPrice);
+}
