3e2957a92d9825cdbccfc2e00053928ff1d59433
Jauhen Feldman <feldmeow@gmail.com>
2022-03-05 00:55:25 +0300

    fix: rebuild js file

--- lesson08/02/game01.js
+++ lesson08/02/game01.js
@@ -1,56 +1,65 @@
 'use strict';
-{
-	const isNumber = function (n) {
-		return !isNaN(parseFloat(n));
-	};
-	function hiddenNumber() {
-		const hiddenNum = Math.floor(Math.random() * 100); //генерация числа
-		let count = 10;
-		function getNum() {
-			let number = prompt(
-				`Угадай число от 1 до 100 (Осталось попыток: ${count})`
-			); //запрос числа
-			const isRetry = () => {
-				if (confirm('Хотите сыграть ещё?')) {
-					hiddenNumber();
-				} else {
-					alert('До свидания!');
-				}
-			};
-			function result() {
-				function alertMessage() {
-					if (+number === hiddenNum) {
-						alert('Вы угадали!');
-						isRetry();
-					} else if (!isNumber(number)) {
-						count -= 1;
-						alert('Введи число!');
-					} else if (number > hiddenNum) {
-						count -= 1;
-						alert('Загаданное число меньше');
-					} else if (number < hiddenNum) {
-						count -= 1;
-						alert('Загаданное число больше');
-					}
-				}
-				if (+number === hiddenNum) {
-					return alertMessage();
-				} else if (number === null) {
-					alert('До свидания!');
-					return false;
-				} else {
-					alertMessage();
-					if (count > 0) {
-						getNum();
-					} else {
-						alert('Попытки закончились :(');
-						isRetry();
-					}
-				}
-			}
-			result();
-		}
-		getNum();
+
+const isNumber = function (n) {
+	return !isNaN(parseFloat(n));
+};
+
+const generateHiddenNumber = () => {
+	const hiddenNum = Math.floor(Math.random() * 100);
+	console.log(hiddenNum);
+	return hiddenNum;
+};
+
+const getNumber = (count) => {
+	return prompt(`Угадай число от 1 до 100 (Осталось попыток: ${count})`);
+};
+
+const checkNumber = (num, hNum) => {
+	num = Number(num);
+	if (num === hNum) {
+		alert('Вы угадали!');
+		return false;
+	} else if (num > hNum) {
+		alert('Загаданное число меньше');
+		return true;
+	} else if (num < hNum) {
+		alert('Загаданное число больше');
+		return true;
 	}
-	hiddenNumber();
-}
+};
+
+const isRetry = () => {
+	if (confirm('Хотите сыграть ещё?')) {
+		start();
+	} else {
+		alert('До свидания!');
+	}
+};
+
+const start = () => {
+	let count = 10;
+	let currentNumber;
+	const hiddenNumber = generateHiddenNumber();
+	st: while (count > 0) {
+		currentNumber = getNumber(count);
+		count--;
+		if (currentNumber === null) {
+			isRetry();
+			break st;
+		} else if (!isNumber(currentNumber)) {
+			alert('Введи число!');
+			continue st;
+		} else {
+			if (!checkNumber(currentNumber, hiddenNumber)) {
+				isRetry();
+				break st;
+			}
+		}
+	}
+	if (count < 1) {
+		alert('Попытки закончились!');
+		isRetry();
+	}
+};
+
+start();
