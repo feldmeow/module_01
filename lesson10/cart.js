6f6ccb41f681cb2c390c9e5e6e393421b54a1ffd
Jauhen Feldman <feldmeow@gmail.com>
2022-03-12 18:19:11 +0300

    fix: codestyle rules

--- lesson10/cart.js
+++ lesson10/cart.js
@@ -1,39 +1,39 @@
 'use strict';
 
 const cart = {
-	items: [],
-	count: 0,
-	get totalPrice() {
-		return this.calculateItemPrice();
-	},
+  items: [],
+  count: 0,
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
 
-	calculateItemPrice() {
-		let price = 0;
-		this.items.forEach((item) => {
-			price += item.price * item.count;
-		});
-		return price;
-	},
+  calculateItemPrice() {
+    let price = 0;
+    this.items.forEach((item) => {
+      price += item.price * item.count;
+    });
+    return price;
+  },
 
-	increaseCount(x) {
-		this.count += x;
-	},
+  increaseCount(x) {
+    this.count += x;
+  },
 
-	add(item, price, count = 1) {
-		this.items.push({ item: item, price: price, count: count });
-		this.increaseCount(count);
-	},
+  add(item, price, count = 1) {
+    this.items.push({item, price, count});
+    this.increaseCount(count);
+  },
 
-	clear() {
-		this.items.length = 0;
-		this.totalPrice = 0;
-		this.count = 0;
-	},
+  clear() {
+    this.items.length = 0;
+    this.totalPrice = 0;
+    this.count = 0;
+  },
 
-	print() {
-		console.log(JSON.stringify(this.items));
-		console.log(this.totalPrice);
-	},
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(this.totalPrice);
+  },
 };
 
 cart.add('ноутбук', 1000, 2);
